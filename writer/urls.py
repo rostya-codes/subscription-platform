6e99619b3bf21974979490580aed527d17018dd2
Rostya <rostyamudrik@proton.me>
2025-03-08 16:20:15 +0100

    my articles added

--- writer/urls.py
+++ writer/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('writer-dashboard', views.writer_dashboard, name='writer-dashboard'),
     path('create-article', views.create_article, name='create-article'),
+    path('my_articles', views.my_articles, name='my-articles'),
 ]
--- writer/views.py
+++ writer/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 
 from . forms import ArticleForm
+from . models import Article
 
 
 @login_required(login_url='my-login')
@@ -22,8 +23,17 @@ def create_article(request):
             article.user = request.user
             article.save()
 
-            return HttpResponse('Article created!')
+            return redirect('my-articles')
 
     context = {'CreateArticleForm': form}
 
     return render(request, 'writer/create-article.html', context)
+
+
+@login_required(login_url='my-login')
+def my_articles(request):
+    current_user = request.user.id
+    articles = Article.objects.all().filter(user=current_user)
+    context = {'AllArticles': articles}
+
+    return render(request, 'writer/my-articles.html', context)
