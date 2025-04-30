ff925c8baa406509e05b72814d38dc0fa7aaf794
Rostya <rostyamudrik@proton.me>
2025-04-30 14:12:16 +0200

    reset password is done

--- account/urls.py
+++ account/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
@@ -7,5 +8,15 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('register', views.register, name='register'),
     path('my-login', views.my_login, name='my-login'),
-    path('user-logout', views.user_logout, name='user-logout')
+    path('user-logout', views.user_logout, name='user-logout'),
+
+    # Password management
+    # 1 - Allow us to enter our email in order to receive a password reset link
+    path('reset-password', auth_views.PasswordResetView.as_view(template_name='account/password-reset.html'), name='reset_password'),
+    # 2 - Show a success message stating that an email was sent to reset our password
+    path('reset_password_sent', auth_views.PasswordResetDoneView.as_view(template_name='account/password-reset-sent.html'), name='password_reset_done'),
+    # 3 - Send a link to our email, so that we can reset our password + We will be prompted to enter a new password
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='account/password-reset-form.html'), name='password_reset_confirm'),
+    # 4 - Show a success message stating that our password was changed
+    path('password_reset_coplete', auth_views.PasswordResetCompleteView.as_view(template_name='account/password-reset-complete.html'), name='password_reset_complete'),
 ]
--- subplatform/settings.py
+++ subplatform/settings.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+from config import email_password
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -128,3 +130,15 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'rostyamudrik@gmail.com'
+EMAIL_HOST_PASSWORD = email_password
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
